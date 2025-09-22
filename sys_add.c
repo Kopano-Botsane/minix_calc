27f6deb213fa8363d061c879351a2b24095142ef
Kopano-Botsane <kopanoabotsane@gmail.com>
2025-09-22 23:30:00 +0200

    Update sys_add.c

--- sys_add.c
+++ sys_add.c
@@ -5,19 +5,17 @@
 #include <lib.h>
 #include <minix/rs.h>
 #include <minix/syslib.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <errno.h>
 
-/* Make the _syscall macro available */
-#define _SYSCALL
+/* Define the message structure for our add call */
+#define CALC_ADD_REQUEST   1
 
-/* The public library function */
+/* Public library function - user programs will call this */
 int sys_add(int a, int b)
 {
+    message m;
     int r;
     endpoint_t calc_ep;
-    message m;
     
     /* Look up the endpoint of the calc server */
     r = minix_rs_lookup("calc", &calc_ep);
@@ -26,21 +24,19 @@ int sys_add(int a, int b)
         return -1;
     }
     
-    /* Prepare the message - we'll use generic fields for now */
-    m.m_type = ADD;  /* Use the ADD call number we defined in callnr.h */
+    /* Prepare the message */
+    m.m_type = CALC_ADD_REQUEST;
+    m.m1_i1 = a;  /* First operand */
+    m.m1_i2 = b;  /* Second operand */
     
-    /* Store operands in generic message fields */
-    m.m1_i1 = a;     /* First operand */
-    m.m1_i2 = b;     /* Second operand */
-    
-    /* Send the message to the calc server and wait for reply */
-    r = _taskcall(calc_ep, ADD, &m);
+    /* Send the message to the calc server using the kernel call mechanism */
+    r = _taskcall(calc_ep, CALC_ADD_REQUEST, &m);
     
     if (r != OK) {
         errno = r;
         return -1;
     }
     
-    /* Return the result from the reply message */
+    /* Return the result from the server */
     return m.m1_i1;  /* Result should be in m1_i1 */
 }
